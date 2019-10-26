ea401e7cf4caa19c8a7211526551501cc628c026
scott88lee <scott88lee@gmail.com>
2019-10-26 18:37:00 +0800

    connected PSQL to backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,36 @@
 const express = require('express');
+const pg = require('pg');
+
 const app = express();
 
+// Initialise postgres client
+const config = {
+  host: '127.0.0.1',
+  database: 'sam-dev',
+  port: 5432
+};
+
+const pool = new pg.Pool(config);
+
+pool.on('error', function (err) {
+  console.log('idle client error', err.message, err.stack);
+});
+
 app.get('*', (request, response) => {
-  response.send('hello brian');
+  
+const queryString = 'SELECT * from users'
+pool.query(queryString, (err, result) => {
+
+  if (err) {
+    console.error('query error:', err.stack);
+    response.send( 'query error' );
+  } else {
+    console.log('query result:', result);
+
+    // redirect to home page
+    response.send( result.rows );
+  }
+});
 });
 
 app.listen(3000);
