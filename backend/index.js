4d7748552696232464892aa1b3d3821864080d9b
scott88lee <scott88lee@gmail.com>
2019-10-28 17:59:34 +0800

    Got a working SQL interface

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const pg = require('pg');
-
 const app = express();
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded( {extended: true} )); //Parse URL-encoded bodies
 
 // Initialise postgres client
+const pg = require('pg');
 const config = {
   host: '127.0.0.1',
   database: 'sam-dev',
@@ -17,20 +18,54 @@ pool.on('error', function (err) {
 });
 
 app.get('*', (request, response) => {
-  
-const queryString = 'SELECT * from users'
-pool.query(queryString, (err, result) => {
+  const queryString = 'SELECT * from users';
+  pool.query(queryString, (err, result) => {
+    if (err) {
+      console.error('query error:', err.stack);
+      response.send( 'query error' );
+    } else {
+      console.log('query result:', result);
 
-  if (err) {
-    console.error('query error:', err.stack);
-    response.send( 'query error' );
-  } else {
-    console.log('query result:', result);
+      // redirect to home page
+      response.send( result.rows );
+    }
+  });
+});
 
-    // redirect to home page
-    response.send( result.rows );
+app.post('/wildcard', async (request, response) => {
+  let data = request.body;
+  console.log(data);
+
+  if (data.mode == 'sql') {
+    try {
+      let result = await SQLquery(data.queryString)
+      console.log(result)
+      if (result) {
+        response.send(result);
+      } else {
+        response.send("No results")
+      }
+    } catch(err) {
+      response.send("SQL Error");
+    }
   }
 });
-});
 
-app.listen(3000);
+function SQLquery(queryString){
+  console.log('Executing: ' + queryString);
+  
+  return new Promise((resolve, reject) => {
+    pool.query(queryString, (err, result) => {
+      if (err) {
+        console.log(err.stack);
+        reject(err.stack);
+      } else {
+        console.log('Query success');
+        //console.log(result.rows);
+        resolve(result.rows);
+      }
+    });
+  })
+}
+
+app.listen(3000, ()=> {console.log("Listen 3000")});
